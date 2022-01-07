bcb83413b63f369fc912d36a8e502e6d947bb679
2031001 <2031001@isi-montreal.com>
2022-01-07 16:53:42 -0500

    Exercise logMsg( )

--- web_server_0/server.js
+++ web_server_0/server.js
@@ -1,13 +1,47 @@
 'use strict'
 
-console.log('NodeJs Interpreter starts executing Javascript code')
+const fs = require('fs')
+const dir = 'log'
+const fileName = 'log/server_log.log'
 
-function add (a, b) {
-    return a + b
+function logMsgSync (msg) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir)
+        console.log('Directory created : ', dir)
+    }
+    try {
+        fs.appendFileSync(fileName, msg + '\n')
+        console.log('File appended')
+    } catch (err) {
+        console.log(err)
+    }
 }
+logMsgSync('This is a test message 1')
 
-const val1 = 2
-const val2 = 6
-const result = add(val1, val2)
-console.log('Hello World')
-console.log('Add function result: ' + result)
+function logMsg (msg) {
+    fs.stat(dir,
+        (err) => {
+            if (err == null) {
+                console.log('file exits')
+            } else if (err.code === 'ENOENT') {
+                fs.mkdir(dir,
+                    (err) => {
+                        if (err) throw err
+                        console.log('Directory created : ', dir)
+                    }
+                )
+            }
+            fs.appendFile(fileName, msg + '\n',
+                (err) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log('File appended')
+                    }
+                }
+            )
+        }
+
+    )
+}
+logMsg('This is a test message 2')
